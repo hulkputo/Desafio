e90c013af7051f85bb7b96c65576ce71378e4f95
Beatriz Nascimento <b.nascimento.luzmoraes@gmail.com>
2021-08-26 00:39:22 -0300

    versão final

--- percorreArquivo.js
+++ percorreArquivo.js
@@ -4,22 +4,31 @@ const Doc = require("./model/doc");
 function percorreArquivo() {
   fs.readFile("./data.csv", "utf-8", async function (err, data) {
     var linhas = data.split(/\r?\n/);
-    
+
     const docs = [];
-    
+
     linhas.forEach(function (linha, cont) {
-      const docInfo = linha.split(',')
-      
-      docs.push({
-        name: docInfo[0],
-        value: docInfo[1]
-      })
+      const docInfo = linha.split(",");
+
+      // verifica se existem nomes/valores vazios no arquivo e se algum valor não é um número
+      if (
+        docInfo[0] !== "" &&
+        docInfo[0] !== undefined &&
+        (docInfo[1] !== "" || docInfo[1] !== undefined)
+      ) {
+        if (!isNaN(parseInt(docInfo[1]))) {
+          docs.push({
+            name: docInfo[0],
+            value: docInfo[1],
+          });
+        }
+      }
     });
-    
+
     const docsJson = await Doc.insertMany(docs);
 
-    console.log(docsJson)
-    console.log('\n\nOK');
+    console.log(docsJson);
+    console.log("\n\nOK");
   });
 }
 
--- readme.md
+++ readme.md
@@ -1,7 +1,16 @@
 # Desafio IOUU
 Para o desenvolvimento do desafio, foi utilizado:
 - Node.js;
-- MongoDB - banco hospedado na nuvem MongoDB Atlas. 
+- MongoDB - banco hospedado no https://cloud.mongodb.com/. 
+
+# Como acessar o banco
+- Para visualizar os dados no banco, é necessário realizar o login no Atlas.
+- Acessar https://cloud.mongodb.com/ ;
+- Realizar o login usando as  credenciais do google.
+    login: desafioiouu@gmail.com
+    senha: desafio1234
+- Assim que logar, clicar em "Browse Collections";
+    Dessa forma, você terá acesso a visualização do que está subindo no banco (Desafio.desafio-inserts).
 
 # Como instalar
 - Clone o projeto utilizando o comando "git clone https://https://github.com/hulkputo/Desafio";
